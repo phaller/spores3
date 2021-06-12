f77d42a30de7c9ced1a31e90cbdd5fa0ed1b3e37
Philipp Haller <hallerp@gmail.com>
2021-06-12 19:18:00 +0200

    Move some tests for JVM/JS to shared pickle tests

--- blocks-upickle/jvm/src/test/scala/com/phaller/blocks/pickle/test/PickleTests.scala
+++ blocks-upickle/jvm/src/test/scala/com/phaller/blocks/pickle/test/PickleTests.scala
@@ -17,45 +17,9 @@ object MyBlock extends Block.Creator[Int, Int, Int](
 class PickleTests {
 
   @Test
-  def testCreator(): Unit = {
-    val c = Creator[Int, Int, Int]("com.phaller.blocks.pickle.test.MyBlock")
-    val s = c(12)
-    val res = s(3)
-    assert(res == 16)
-  }
-
-  @Test
   def testUpickle(): Unit = {
     val res = write(Seq(1, 2, 3))
     assert(res == "[1,2,3]")
-  }
-
-  type BlockT = Block[Int, Int] { type Env = Int }
-
-  @Test
-  def testBlockReadWriter(): Unit = {
-    val name = "com.phaller.blocks.pickle.test.MyBlock"
-    val env: Int = 12
-
-    given blockReadWriter: ReadWriter[BlockT] =
-      readwriter[ujson.Value].bimap[BlockT](
-        // currently, cannot obtain creator name from block `b`
-        b => ujson.Arr(name, b.envir),
-        json => {
-          val n: String = json(0).str
-          val i: Int = json(1).num.toInt
-          val c = Creator[Int, Int, Int](n)
-          c(i)
-        }
-      )
-
-    // create a block
-    val block: BlockT = MyBlock(12)
-
-    // pickle block
-    val res = write(block)
-
-    assert(res == """["com.phaller.blocks.pickle.test.MyBlock",12]""")
   }
 
   @Test
--- blocks-upickle/shared/src/test/scala/com/phaller/blocks/pickle/test/SharedPickleTests.scala
+++ blocks-upickle/shared/src/test/scala/com/phaller/blocks/pickle/test/SharedPickleTests.scala
@@ -19,4 +19,40 @@ class SharedPickleTests {
     assert(res == "[1,2,3]")
   }
 
+  @Test
+  def testCreator(): Unit = {
+    val c = Creator[Int, Int, Int]("com.phaller.blocks.pickle.test.MyBlock")
+    val s = c(12)
+    val res = s(3)
+    assert(res == 16)
+  }
+
+  type BlockT = Block[Int, Int] { type Env = Int }
+
+  @Test
+  def testBlockReadWriter(): Unit = {
+    val name = "com.phaller.blocks.pickle.test.MyBlock"
+    val env: Int = 12
+
+    given blockReadWriter: ReadWriter[BlockT] =
+      readwriter[ujson.Value].bimap[BlockT](
+        // currently, cannot obtain creator name from block `b`
+        b => ujson.Arr(name, b.envir),
+        json => {
+          val n: String = json(0).str
+          val i: Int = json(1).num.toInt
+          val c = Creator[Int, Int, Int](n)
+          c(i)
+        }
+      )
+
+    // create a block
+    val block: BlockT = MyBlock(12)
+
+    // pickle block
+    val res = write(block)
+
+    assert(res == """["com.phaller.blocks.pickle.test.MyBlock",12]""")
+  }
+
 }
