998cfaded47bd8df6def45774235b79d0827d083
Philipp Haller <hallerp@gmail.com>
2021-06-21 10:22:42 +0200

    Finish moving tests for JVM/JS to shared pickle tests

--- blocks-upickle/shared/src/test/scala/com/phaller/blocks/pickle/test/SharedPickleTests.scala
+++ blocks-upickle/shared/src/test/scala/com/phaller/blocks/pickle/test/SharedPickleTests.scala
@@ -4,8 +4,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
 
-import com.phaller.blocks.Block
-import com.phaller.blocks.Creator
+import com.phaller.blocks.{Block, Creator, CBlock}
 
 import upickle.default._
 
@@ -53,6 +52,89 @@ class SharedPickleTests {
     val res = write(block)
 
     assert(res == """["com.phaller.blocks.pickle.test.MyBlock",12]""")
+  }
+
+  @Test
+  def testCBlockReadWriter(): Unit = {
+    given blockReadWriter: ReadWriter[CBlock[Int, Int, Int]] =
+      readwriter[ujson.Value].bimap[CBlock[Int, Int, Int]](
+        cblock => ujson.Arr(cblock.creatorName, cblock.block.envir),
+        json => {
+          val n = json(0).str
+          val i = json(1).num.toInt
+          val c = Creator[Int, Int, Int](n)
+          val cblock = c(i)
+          new CBlock(n, cblock)
+        }
+      )
+
+    // name of creator
+    val name = "com.phaller.blocks.pickle.test.MyBlock"
+
+    // create a CBlock
+    val x = CBlock(12)[Int, Int](name)
+
+    val res = write(x)
+
+    assert(res == """["com.phaller.blocks.pickle.test.MyBlock",12]""")
+  }
+
+  @Test
+  def testGenericCBlockReadWriter(): Unit = {
+    given blockReadWriter[T, R]: ReadWriter[CBlock[Int, T, R]] =
+      readwriter[ujson.Value].bimap[CBlock[Int, T, R]](
+        cblock => ujson.Arr(cblock.creatorName, cblock.block.envir),
+        json => {
+          val n = json(0).str
+          val i = json(1).num.toInt
+          val c = Creator[Int, T, R](n)
+          val cblock = c(i)
+          new CBlock(n, cblock)
+        }
+      )
+
+    // name of creator
+    val name = "com.phaller.blocks.pickle.test.MyBlock"
+
+    // create a CBlock
+    val x = CBlock(12)[Int, Int](name)
+
+    val res = write(x)
+
+    assert(res == """["com.phaller.blocks.pickle.test.MyBlock",12]""")
+  }
+
+  @Test
+  def testFullyGenericCBlockReadWriter(): Unit = {
+    given blockReadWriter[E, T, R](using envReadWriter: ReadWriter[E]): ReadWriter[CBlock[E, T, R]] =
+      readwriter[ujson.Value].bimap[CBlock[E, T, R]](
+        cblock => {
+          val pickledEnv = write(cblock.block.envir)
+          ujson.Arr(cblock.creatorName, pickledEnv)
+        },
+        json => {
+          val creatorName = json(0).str
+          val env = read[E](json(1).str)
+          val c = Creator[E, T, R](creatorName)
+          val block = c(env)
+          new CBlock(creatorName, block)
+        }
+      )
+
+    // FQN of creator
+    val name = "com.phaller.blocks.pickle.test.MyBlock"
+
+    // create a CBlock
+    val x = CBlock(12)[Int, Int](name)
+
+    // pickle CBlock
+    val res = write(x)
+
+    assert(res == """["com.phaller.blocks.pickle.test.MyBlock","12"]""")
+
+    val y = read[CBlock[Int, Int, Int]](res)
+    val res2 = y(3)
+    assert(res2 == 16)
   }
 
 }
